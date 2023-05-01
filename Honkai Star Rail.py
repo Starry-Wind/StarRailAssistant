fa6fea714065fbbd0ef1f377e5cb5cdfdc32498d
Starry-Wind <656433522@qq.com>
2023-05-02 00:36:52 +0800

    代码

--- Honkai Star Rail.py
+++ Honkai Star Rail.py
@@ -6,6 +6,7 @@ def main():
     time.sleep(5)
     print("开始运行，请勿移动鼠标和键盘\n若脚本运行无反应，请使用管理员权限运行")
     map_instance.auto_map_1()  #基座舱段
+    map_instance.auto_map_2()  #收容舱段
     map_instance.auto_map_3()  #支援舱段
     print("脚本已经完成运行")
 
