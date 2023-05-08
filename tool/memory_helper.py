f8b667453a8c6f4266a2fba8849d044710ccd28d
Maikiwi <74925636+maikirakiwi@users.noreply.github.com>
2023-05-08 03:29:24 -0700

    class rewrite

--- tool/memory_helper.py
+++ tool/memory_helper.py
@@ -1,18 +1,23 @@
 import pymem
 import pymem.process
-
-class PlayerLocation:
-    def __init__(self) -> None:
-        try:
-            self.game = pymem.Pymem("StarRail.exe")
-            self.base_address = pymem.process.module_from_name(self.game.process_handle, "unityplayer.dll").lpBaseOfDll
-        except:
-            print("游戏初始化错误")
+class MemoryReader:
+    def __init__(self):
         self.offset_x = 30815264
         self.offset_y = 30815268
         self.offset_z = 30815272
-    def get_xyz(self):
+
+    def init(self):
+        self.game = pymem.Pymem("StarRail.exe")
+        self.base_address = pymem.process.module_from_name(self.game.process_handle, "unityplayer.dll").lpBaseOfDll
+    
+    def close(self):
+        result = pymem.process.close_handle(self.game.process_handle)
+        if result != True:
+            raise Exception("关闭进程句柄失败")
+
+    def get_location(self):
         try:
+            # X, Y, Z
             return self.game.read_float(self.base_address + self.offset_x), self.game.read_float(self.base_address + self.offset_y), self.game.read_float(self.base_address + self.offset_z)
         except:
             print("读取坐标错误（大概率是游戏版本比软件支持的版本高）")
