d7f6b34fa5d6f521fdd631587c1cf6ef6dc0bdb7
Maikiwi <74925636+maikirakiwi@users.noreply.github.com>
2023-05-07 20:25:24 -0700

    更改read_json_file逻辑

--- tool/config.py
+++ tool/config.py
@@ -3,33 +3,35 @@ def normalize_file_path(filename):
     # 尝试在当前目录下读取文件
     current_dir = os.getcwd()
     file_path = os.path.join(current_dir, filename)
+    print(file_path)
     if os.path.exists(file_path):
         return file_path
     else:
         # 如果当前目录下没有该文件，则尝试在上一级目录中查找
         parent_dir = os.path.dirname(current_dir)
         file_path = os.path.join(parent_dir, filename)
+        print(parent_dir)
         if os.path.exists(file_path):
             return file_path
         else:
             # 如果上一级目录中也没有该文件，则返回None
             return None
     
-def read_json_file(filename):
+def read_json_file(filename, path=False):
     # 找到文件的绝对路径
     file_path = normalize_file_path(filename)
     with open(file_path, 'rb') as f:
-        data = orjson.load(f.read())
-        return data, file_path
+        data = orjson.loads(f.read())
+        if path == True:
+            return data, file_path
+        else:
+            return data
 
 def modify_json_file(filename, key, value):
     #先读，再写 
-    data, file_path = read_json_file(filename)
+    data, file_path = read_json_file(filename, path=True)
     data[key] = value
     current_dir = os.getcwd()   
-    with open(file_path, 'w') as f:
+    with open(file_path, 'wb') as f:
         f.write(orjson.dumps(data, indent=4, ensure_ascii=False))
         return 
-
-
-
