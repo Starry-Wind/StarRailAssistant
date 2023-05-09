8c6b2c75203bdef7c1dd1386bd57ed5492291e8b
Night-stars-1 <99261160+Night-stars-1@users.noreply.github.com>
2023-05-09 22:07:11 +0800

    Add files via upload

--- tool/calculated.py
+++ tool/calculated.py
@@ -20,9 +20,7 @@ class calculated:
         self.keyboard = KeyboardController()
 
     def Click(self, points):
-        real_width = self.CONFIG['real_width']
-        real_height = self.CONFIG['real_height']
-        x, y = int(points[0] * 1295 / real_width), int(points[1] * 757 / real_height)
+        x, y = int(points[0]), int(points[1])
         log.debug((x, y))
         win32api.SetCursorPos((x, y))
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
@@ -34,7 +32,8 @@ class calculated:
         left, top, right, bottom = win32gui.GetWindowRect(hwnd)
         real_width = self.CONFIG['real_width']
         real_height = self.CONFIG['real_height']
-        x, y = int((left + points[0]) * 1295 / real_width), int((top + points[1]) * 757 / real_height)
+        x, y = int(left + points[0]), int(top + points[1])
+        log.info((x, y))
         log.debug((x, y))
         win32api.SetCursorPos((x, y))
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
@@ -123,7 +122,7 @@ class calculated:
             log.info(f"执行{map_filename}文件:{map_index+1}/{len(map_data['map'])} {map}")
             key = list(map.keys())[0]
             value = map[key]
-            if key in ['w', 's', 'a', 'd']:
+            if key in ['w', 's', 'a', 'd', 'f']:
                 self.keyboard.press(key)
                 start_time = time.perf_counter()
                 while time.perf_counter() - start_time < value:
@@ -131,13 +130,6 @@ class calculated:
                 self.keyboard.release(key)
             elif key == "mouse_move":
                 self.Mouse_move(value)
-            elif key == "f":
-                self.keyboard.press(key)
-                start_time = time.perf_counter()
-                while time.perf_counter() - start_time < value:
-                    pass
-                self.keyboard.release(key)
-                time.sleep(3) # 防止因为传送导致脚本卡死
             elif key == "fighting":
                 if value == 1:      # 进战斗
                     self.fighting()
@@ -154,3 +146,4 @@ class calculated:
         # 该公式为不同缩放比之间的转化
         dx = int(x * 1295 / real_width)
         win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, dx, 0)  # 进行视角移动
+        time.sleep(0.5)
--- tool/map.py
+++ tool/map.py
@@ -5,7 +5,6 @@ import time
 from .config import get_file
 from .log import log
 
-
 class map:
     def __init__(self):
         self.calculated = calculated()
@@ -28,28 +27,29 @@ class map:
     def auto_map(self, start):
         map_list = get_file('./map', 'old')
         if f'map_{start}.json' in map_list:
-            map_list = map_list[map_list.index(
-                f'map_{start}.json'):len(map_list)]
+            map_list = map_list[map_list.index(f'map_{start}.json'):len(map_list)]
             for map in map_list:
                 # 选择地图
-                pyautogui.keyDown("m")
-                pyautogui.keyUp("m")
-                time.sleep(1)
-                self.map_init()
                 map = map.split('.')[0]
                 map_data = read_json_file(f"map\\{map}.json")
-                name = map_data['name']
-                author = map_data['author']
-                start_dict = map_data['start']
+                name=map_data['name']
+                author=map_data['author']
+                start_dict=map_data['start']
                 log.info(f"开始\033[0;34;40m{name}\033[0m锄地")
-                log.info(f"改路线导航作者：\033[0;31;40m{author}\033[0m")
+                log.info(f"该路线导航作者：\033[0;31;40m{author}\033[0m")
                 log.info(f"感谢每一位无私奉献的作者")
                 for start in start_dict:
                     key = list(start.keys())[0]
                     log.debug(key)
                     value = start[key]
-                    time.sleep(value)
-                    self.calculated.click_target(key, 0.85)
+                    if key == 'map':
+                        pyautogui.keyDown("m")
+                        pyautogui.keyUp("m")
+                        time.sleep(1)
+                        self.map_init()
+                    else:
+                        time.sleep(value)
+                        self.calculated.click_target(key, 0.98)
                 time.sleep(5)
                 self.calculated.auto_map(map, False)
         else:
