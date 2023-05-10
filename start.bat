d83fc88b4a4e341a22aacb341ad54377c3b9c666
apple050620312 <67982686+apple050620312@users.noreply.github.com>
2023-05-10 15:48:15 +0800

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,4 +1,38 @@
-@echo off
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFA5HSRa+GGStCLkT6ezo08KIrEYcXa8MbI7Jl4aPKexe6EDwdNt/mC1ljM4fGCdZbQaqIAY3pg4=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFA5HSRa+GGStCLkT6ezo08KIrEYcXa8MbI7Jl4aPKexe6EDwdNt/mC1ljM4fGCdwcRygbwZ0mnpDpCS3Ns6Q/Qr5Tyg=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+python --version
+pip install -r requirements.txt
 :: BatchGotAdmin (Run as Admin code starts)
 REM --> Check for permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
