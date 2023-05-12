7ec72463a4ab5490b2e392e6cc85f3545e99fe8d
三哥哥 <apple050620312@gmail.com>
2023-05-12 21:05:53 +0800

    Update Honkai_Star_Rail.py

--- Honkai_Star_Rail.py
+++ Honkai_Star_Rail.py
@@ -29,20 +29,23 @@ def main():
         # asyncio.run(check_file(ghproxy, "temp"))
         update_file_main(url_proxy=ghproxy)
     if isadmin() == 1:
-        start = input('请输入起始地图（如果从头开始请输入0）：')
-        if "-" in start and "_" in start or start == '0':
-            log.info("脚本将于5秒后运行,请确保你的游戏置顶")
-            time.sleep(5)
-            get_width()
-            from tool.map import map
-            map_instance = map()
-            log.info("开始运行，请勿移动鼠标和键盘")
-            log.info("若脚本运行无反应,请使用管理员权限运行")
-            start = '1-1_1' if start == '0' else start
-            map_instance.auto_map(start)  # 读取配置
+        start = input('请输入起始地图（如果要从头开始请回車）：')
+        if start.isdigit() == True:
+            start = (start)+'-1_1'
+        elif "-" in start:
+            start = (start)+'_1'
         else:
             log.info("错误编号")
-        webhook_and_log("脚本已经完成运行")
+            webhook_and_log("脚本已经完成运行")
+        log.info("脚本将于5秒后运行,请确保你的游戏置顶")
+        time.sleep(5)
+        get_width()
+        from tool.map import map
+        map_instance = map()
+        log.info("开始运行，请勿移动鼠标和键盘")
+        log.info("若脚本运行无反应,请使用管理员权限运行")
+        start = '1-1_1' if start == '' else start
+        map_instance.auto_map(start)  # 读取配置
     else:
          log.info("请以管理员权限运行")
 
