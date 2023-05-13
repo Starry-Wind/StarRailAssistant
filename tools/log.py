f2675fe82efcab032fc66c50632c29fad280f0d2
Night-stars-1 <nujj1042633805@gmail.com>
2023-05-14 01:19:19 +0800

    fix: name 'read_json_file' is not defined

--- tools/log.py
+++ tools/log.py
@@ -2,8 +2,8 @@
 Author: Night-stars-1 nujj1042633805@gmail.com
 Date: 2023-05-12 23:22:54
 LastEditors: Night-stars-1 nujj1042633805@gmail.com
-LastEditTime: 2023-05-13 01:00:33
-FilePath: \Honkai-Star-Rail-beta-2.4h:\Download\Zip\Honkai-Star-Rail-beta-2.7\tool\log.py
+LastEditTime: 2023-05-14 01:18:53
+FilePath: \Honkai-Star-Rail-beta-2.4h:\Download\Zip\Honkai-Star-Rail-beta-2.7\tools\log.py
 Description: 
 
 Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
@@ -14,8 +14,10 @@ from loguru import logger
 
 try:
     from .requests import post
+    from .config import read_json_file # Circular import
 except:
     from requests import post
+    from config import read_json_file # Circular import
 
 VER = "3.0"
 log = logger
@@ -35,7 +37,6 @@ logger.add(path_log,
 
 def webhook_and_log(message):
     log.info(message)
-    from tool.config import read_json_file # Circular import
     url = read_json_file("config.json", False).get("webhook_url")
     if url == "" or url == None:
         return
