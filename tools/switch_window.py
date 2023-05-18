d037d409373c456f89ef4b5da8c63476d16f07d3
雪见璃 <75160294+snowly233@users.noreply.github.com>
2023-05-18 12:27:21 +0800

    fix:bug

--- tools/switch_window.py
+++ tools/switch_window.py
@@ -1,5 +1,5 @@
 import pyautogui
-
+from win32com import client
 def switch_window(title = '崩坏：星穹铁道'):
 
 	ws = pyautogui.getWindowsWithTitle(title)
@@ -9,6 +9,7 @@ def switch_window(title = '崩坏：星穹铁道'):
 			# 避免其他窗口也包含崩坏：星穹铁道，比如正好开着github脚本页面
 			# print(w.title)
 			if w.title == title:
+				client.Dispatch("WScript.Shell").SendKeys('%')
 				w.activate()
 				break
 	else:
