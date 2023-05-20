e130c0ec48a3ed21fafda8cdfee1a6ab2b093bc0
Starry-Wind <122146834+Starry-Wind@users.noreply.github.com>
2023-05-21 02:15:54 +0800

    Revert "fix: 使用win32gui激活窗口，hwnd检验"

--- tools/switch_window.py
+++ tools/switch_window.py
@@ -1,16 +1,19 @@
-import keyboard
-import win32gui
-from tools.log import log
+import pyautogui
+from win32com import client
+def switch_window(title = '崩坏：星穹铁道'):
 
-
-def switch_window(class_name='UnityWndClass', title='崩坏：星穹铁道'):
-    hwnd = win32gui.FindWindow(class_name, title)
-    if not hwnd:
-        log.warning("未发现游戏,请完全启动游戏后输入：1")
-        keyboard.wait("1")
-        switch_window()
-    win32gui.SetForegroundWindow(hwnd)
-
+	ws = pyautogui.getWindowsWithTitle(title)
+	
+	if len(ws) >= 1 :
+		for w in ws:
+			# 避免其他窗口也包含崩坏：星穹铁道，比如正好开着github脚本页面
+			# print(w.title)
+			if w.title == title:
+				client.Dispatch("WScript.Shell").SendKeys('%')
+				w.activate()
+				break
+	else:
+		print(f'没找到窗口{title}')
 
 if __name__ == '__main__':
-    switch_window()
+	switch_window()
