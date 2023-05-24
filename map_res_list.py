8033f8280c942c0cb0b2a9afaeb14edab50390f9
Night-stars-1 <99261160+Night-stars-1@users.noreply.github.com>
2023-05-25 01:25:45 +0800

    Update map_res_list.py

--- map_res_list.py
+++ map_res_list.py
@@ -12,16 +12,18 @@ temp_list = []
 this_path = str(Path(__file__).parent)
 
 for file in map_path.rglob('*'):
-    map_list.append({
-        'path': str(file).replace(this_path, '').replace('\\', '/').lstrip('/'),
-        'hash': hashlib.md5(file.read_bytes()).hexdigest()
-    })
+    if os.path.isfile(file):
+        map_list.append({
+            'path': str(file).replace(this_path, '').replace('\\', '/').lstrip('/'),
+            'hash': hashlib.md5(file.read_bytes()).hexdigest()
+        })
 
 for file in temp_path.rglob('*'):
-    temp_list.append({
-        'path': str(file).replace(this_path, '').replace('\\', '/').lstrip('/'),
-        'hash': hashlib.md5(file.read_bytes()).hexdigest()
-    })
+    if os.path.isfile(file):
+        map_list.append({
+            'path': str(file).replace(this_path, '').replace('\\', '/').lstrip('/'),
+            'hash': hashlib.md5(file.read_bytes()).hexdigest()
+        })
 
 with open('map_list.json', 'w', encoding='utf-8') as f:
     json.dump(map_list, f, ensure_ascii=False, indent=2)
