f6d8b7abf7a46b7551abaa435f6ab7a1e60f4e88
Night-stars-1 <99261160+Night-stars-1@users.noreply.github.com>
2023-05-24 08:29:10 +0800

    Add files via upload

--- Honkai_Star_Rail.py
+++ Honkai_Star_Rail.py
@@ -1,5 +1,6 @@
 import os
 import traceback
+
 try:
     from tools.log import log, webhook_and_log
     import time
@@ -7,31 +8,49 @@ try:
     import pyuac
     import questionary
 
-    from get_width import get_width
+    from get_width import get_width, check_mult_screen
     from tools.config import read_json_file, modify_json_file, init_config_file, CONFIG_FILE_NAME
+    from tools.map import Map
     from tools.update_file import update_file_main
     from tools.switch_window import switch_window
     from tools.exceptions import Exception
 except:
-    print(traceback.format_exc())
+    pass
+
+
+def choose_map(map_instance: Map):
+    title_ = "请选择起始星球："
+    options_map = {"空间站「黑塔」": "1", "雅利洛-VI": "2", "仙舟「罗浮」": "3"}
+    option_ = questionary.select(title_, list(options_map.keys())).ask()
+    main_map = options_map.get(option_)
+    title_ = "请选择起始地图："
+    options_map = map_instance.map_list_map.get(main_map)
+    if not options_map:
+        return
+    keys = list(options_map.keys())
+    values = list(options_map.values())
+    option_ = questionary.select(title_, values).ask()
+    side_map = keys[values.index(option_)]
+    return f"{main_map}-{side_map}"
+
 
 def main():
     main_start()
-    start = input('请输入起始地图（如果从头开始请输入0）：')
-    if "-" in start and "_" in start or start == '0':
+    map_instance = Map()
+    start = choose_map(map_instance)
+    if start:
         log.info("脚本将自动切换至游戏窗口，请保持游戏窗口激活")
+        check_mult_screen()
         switch_window()
         time.sleep(0.5)
         get_width()
-        from tools.map import map
-        map_instance = map()
         log.info("开始运行，请勿移动鼠标和键盘")
         log.info("若脚本运行无反应,请使用管理员权限运行")
-        start = '1-1_1' if start == '0' else start
         map_instance.auto_map(start)  # 读取配置
     else:
-        log.info("错误编号")
+        log.info("错误编号，请尝试检查更新")
         webhook_and_log("脚本已经完成运行")
+
 
 def main_start():
     if not read_json_file(CONFIG_FILE_NAME, False).get('start'):
@@ -49,13 +68,15 @@ def main_start():
         modify_json_file(CONFIG_FILE_NAME, "auto_battle_persistence", options.index(option))
         modify_json_file(CONFIG_FILE_NAME, "start", True)
 
+
 def up_data():
+    main_start()    # 无config直接更新时初始化config文件
     if not read_json_file(CONFIG_FILE_NAME, False).get('map_debug'):
         ghproxy = read_json_file(CONFIG_FILE_NAME, False).get('github_proxy', "")
         if "rawgithub_proxy" not in read_json_file(CONFIG_FILE_NAME, False):
-            init_config_file(0,0)
-            raise Exception(f"未检测到必要更新，强制更新脚本，请重新运行脚本")
-    
+            init_config_file(0, 0)
+            raise Exception("未检测到必要更新，强制更新脚本，请重新运行脚本")
+
         rawghproxy = read_json_file(CONFIG_FILE_NAME, False).get('rawgithub_proxy', "")
         # asyncio.run(check_file(ghproxy, "map"))
         # asyncio.run(check_file(ghproxy, "temp"))
@@ -66,11 +87,11 @@ def up_data():
                 'skip_verify': False,
                 'type': "star",
                 'version': "beta-2.7_test",
-                'url_zip': "https://github.com/Starry-Wind/SRA/archive/refs/heads/beta-2.7_test.zip",
+                'url_zip': "https://github.com/Starry-Wind/Honkai-Star-Rail/archive/refs/heads/beta-2.7_test.zip",
                 'unzip_path': ".",
-                'keep_folder': ['.git','logs','temp','map','tmp','venv'],
-                'keep_file': ['config.json','version.json','star_list.json'],
-                'zip_path': "SRA-beta-2.7_test/",
+                'keep_folder': ['.git', 'logs', 'temp', 'map', 'tmp', 'venv'],
+                'keep_file': ['config.json', 'version.json', 'star_list.json'],
+                'zip_path': "Honkai-Star-Rail-beta-2.7_test/",
                 'name': "脚本"
             },
             {
@@ -79,11 +100,11 @@ def up_data():
                 'skip_verify': False,
                 'type': "map",
                 'version': "map",
-                'url_zip': "https://github.com/Starry-Wind/SRA/archive/refs/heads/map.zip",
+                'url_zip': "https://github.com/Starry-Wind/Honkai-Star-Rail/archive/refs/heads/map.zip",
                 'unzip_path': "map",
                 'keep_folder': [],
                 'keep_file': [],
-                'zip_path': "SRA-map/",
+                'zip_path': "Honkai-Star-Rail-map/",
                 'name': "地图"
             },
             {
@@ -92,11 +113,11 @@ def up_data():
                 'skip_verify': False,
                 'type': "temp",
                 'version': "map",
-                'url_zip': "https://github.com/Starry-Wind/SRA/archive/refs/heads/map.zip",
+                'url_zip': "https://github.com/Starry-Wind/Honkai-Star-Rail/archive/refs/heads/map.zip",
                 'unzip_path': "temp",
                 'keep_folder': [],
                 'keep_file': [],
-                'zip_path': "SRA-map/",
+                'zip_path': "Honkai-Star-Rail-map/",
                 'name': "图片"
             },
         ]
@@ -104,6 +125,7 @@ def up_data():
             if up["name"] == "脚本" and read_json_file(CONFIG_FILE_NAME, False).get('script_debug', False):
                 continue
             update_file_main(**up)
+
 
 if __name__ == "__main__":
     try:
