6d49e6c7c3ee4533c8f03bc788977b6fe27418fa
Night-stars-1 <nujj1042633805@gmail.com>
2023-05-25 15:01:54 +0800

    chore: 添加自定义adb的IP端口

--- test.py
+++ test.py
@@ -2,72 +2,84 @@
 Author: Night-stars-1 nujj1042633805@gmail.com
 Date: 2023-05-25 12:54:10
 LastEditors: Night-stars-1 nujj1042633805@gmail.com
-LastEditTime: 2023-05-25 14:09:23
+LastEditTime: 2023-05-25 14:54:11
 Description: 
 
 Copyright (c) 2023 by Night-stars-1, All Rights Reserved. 
 '''
-import PySimpleGUI as sg    
-from hashlib import sha1
-from loguru import logger
-from utils.log import log,info
-import os,shutil
-#首先设置窗口样式
-import sys
-import traceback
-def get_cur_info():
+import PySimpleGUI as psg
 
-    """Return the frame object for the caller's stack frame."""
 
-    try:
-
-        raise Exception
-
-    except:
-
-        f = sys.exc_info()[2].tb_frame.f_back
-
-    return (f.f_code.co_name, f.f_lineno,traceback.extract_stack()[-2].filename.split("\\")[-1].split(".")[0])
-
-def callfunc():
-
-    print(get_cur_info())
-
-if __name__ == '__main__':
-    callfunc()
-
-'''
 #设置窗体里面的内容，是以列表样式
-layout = [
-    [sg.Text('请输入站号：',size=15),sg.InputText(key='-INzhaohao-',size=(20,1))],
-    [sg.Text('程序运行记录',justification='center')],
-    [sg.Output(size=(70, 20),font=("宋体", 10))], 
-    [sg.Button('查询'), sg.Exit()]
-]  
-#窗口实例化 并设置窗口名，把内容放进去    
-window = sg.Window('区域站要素查询', layout)  
-#主题循环
+'''
+psg.set_options(font=("Arial Bold",14))
+l1=psg.Text("Enter Name")
+l2=psg.Text("Faculty")
+l3=psg.Text("Subjects")
+l4=psg.Text("Category")
+l5=psg.Multiline(" ", expand_x=True, key='-OUT-', expand_y=True,justification='left')
+t1=psg.Input("", key='-NM-')
+rb=[]
+rb.append(psg.Radio("Arts", "faculty", key='arts', enable_events=True,default=True))
+rb.append(psg.Radio("Commerce", "faculty", key='comm', enable_events=True))
+rb.append(psg.Radio("Science", "faculty", key='sci',enable_events=True))
+cb=[]
+cb.append(psg.Checkbox("History", key='s1'))
+cb.append(psg.Checkbox("Sociology", key='s2'))
+cb.append(psg.Checkbox("Economics", key='s3'))
+a1=psg.Button("大世界")
+a2=psg.Button("模拟宇宙")
+a3=psg.Button("检查更新")
+b1=psg.Button("OK")
+b2=psg.Button("Exit")
+layout=[[l1, t1],[rb],[cb],[b1, l5, b2]]
+window = psg.Window('星穹铁道小助手', layout, size=(715,250))
 while True:
-    #读取按键和各个插件的值    window.read()窗口显示
-    #event获取按键值
-    #values[‘控件的KEY’]                    
     event, values = window.read()
-    #print(event, values)      
-    if event in (None, 'Exit'):      
-        break      
-    elif event == '查询':
-        shuju=None
-        if shuju == None:
-            #print('提示','找不到'+str(values['-INzhaohao-'])+'站的数据')
-            info("eeee")
-        else:
-            sg.Print('This text is white on a green background', text_color='white', background_color='green', font='Courier 10')
-            sg.Print('The first call sets some window settings like font that cannot be changed')
-            sg.Print('This is plain text just like a print would display')
-            sg.Print('White on Red', background_color='red', text_color='white')
-            sg.Print('The other print', 'parms work', 'such as sep', sep=',')
-            sg.Print('To not extend a colored line use the "end" parm', background_color='blue', text_color='white', end='')
-            sg.Print('\nThis line has no color.')
-#窗口关闭
+    print (event, values)
+    if event in (psg.WIN_CLOSED, 'Exit'): break
+    if event == 'comm':
+        if values['comm']==True:
+            window['s1'].update(text="Accounting")
+            window['s2'].update(text="Business Studies")
+            window['s3'].update(text="Statistics")
+    if event == 'sci':
+        if values['sci']==True:
+            window['s1'].update(text="Physics")
+            window['s2'].update(text="Mathematics")
+            window['s3'].update(text="Biology")
+    if event == 'arts':
+        if values['arts']==True:
+            window['s1'].update(text="History")
+            window['s2'].update(text="Sociology")
+            window['s3'].update(text="Economics")
+    if event=='OK':
+        print(265151)
+        subs=[x.Text for x in cb if x.get()==True]
+        fac=[x.Text for x in rb if x.get()==True]
+        out="""
+        Name={}
+        Faculty: {}
+        Subjects: {}
+        """.format(values['-NM-'], fac[0], " ".join(subs))
+        print(out)
+        window['-OUT-'].update(out)
 window.close()
 '''
+psg.set_options(font=("Arial Bold",14))
+a1=psg.Button("大世界")
+a2=psg.Button("模拟宇宙")
+a3=psg.Button("检查更新")
+layout=[[a1, a2, a3]]
+window = psg.Window('星穹铁道小助手', layout, size=(310,100))
+while True:
+    event, values = window.read()
+    print (event, values)
+    if event in (psg.WIN_CLOSED, 'Exit'): break
+    if event=='大世界':
+        ...
+    if event=='模拟宇宙':
+        ...
+    if event=='检查更新':
+        ...
+window.close()
