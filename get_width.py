3f139ff369e4dfe143937f8dfb58881e6cf5c7fb
Night-stars-1 <nujj1042633805@gmail.com>
2023-06-11 01:09:23 +0800

    chore: 修改分辨率显示

--- get_width.py
+++ get_width.py
@@ -2,7 +2,7 @@
 Author: Night-stars-1 nujj1042633805@gmail.com
 Date: 2023-05-23 17:39:27
 LastEditors: Night-stars-1 nujj1042633805@gmail.com
-LastEditTime: 2023-06-06 11:23:43
+LastEditTime: 2023-06-11 01:09:23
 Description: 
 
 Copyright (c) 2023 by Night-stars-1, All Rights Reserved. 
