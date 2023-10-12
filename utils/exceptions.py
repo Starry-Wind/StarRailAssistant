d0cc8d45dab9cee93a73ed74c9bf72bb502de294
Night-stars-1 <99261160+Night-stars-1@users.noreply.github.com>
2023-10-12 16:59:44 +0800

    Revert "Revert "add: 遗器OCR异常""
    This reverts commit fc21361850ec208a968c61c597fe1b80123fc1ce.

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -25,3 +25,9 @@ class TypeError(Exception):
     def __init__(self, message):
         super().__init__(message)
         log.error(message)
+
+class RelicOCRException(Exception):
+    
+    def __init__(self, message):
+        super().__init__(message)
+        log.error(message)
